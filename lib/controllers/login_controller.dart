36ca094797481e6a8888512845e17c6d24570fb5
kushal <qoosal23.kg@gmail.com>
2021-10-22 22:33:20 +0545

    Chat app completed with firebase authentication, firestore and GetX

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -3,24 +3,25 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class LoginController extends GetxController{
+class LoginController extends GetxController {
   final _auth = FirebaseAuth.instance;
   RxBool showSpinner = false.obs;
 
-  void signInUser({required String emailAddress, required String password}) async {
+  void signInUser(
+      {required String emailAddress, required String password}) async {
     // true
     showSpinner.toggle();
-    try{
-      final result = await _auth.signInWithEmailAndPassword(email: emailAddress, password: password);
-      if(result.user != null) {
+    try {
+      final result = await _auth.signInWithEmailAndPassword(
+          email: emailAddress, password: password);
+      if (result.user != null) {
         Get.offAllNamed(ChatScreen.id);
       }
-    }
-    catch(e) {
-      Get.snackbar("Error", "Login unsuccessful.", backgroundColor: Colors.black38);
+    } catch (e) {
+      Get.snackbar("Error", "Login unsuccessful.",
+          backgroundColor: Colors.black38);
       showSpinner(false);
       throw Exception(e);
     }
   }
-
 }
